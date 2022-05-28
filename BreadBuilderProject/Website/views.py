a31406c7ef9c2d0fb90a6fa2daaa069567d6cf04
jandrealyn <me.larissa.smith@gmail.com>
2022-05-28 20:00:24 +1200

    oop

--- BreadBuilderProject/Website/views.py
+++ BreadBuilderProject/Website/views.py
@@ -1,11 +1,42 @@
-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def welcome():
     return render_template('welcome.html')
+
+@views.route('/home', methods=['GET', 'POST'])
+@login_required
+def home():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+
+    return jsonify({})
 
 @views.route('/about')
 def about():
@@ -15,10 +46,3 @@ def about():
 def quiz():
     return render_template('quiz.html')
 
-@views.route("/home")  # takes you to home page when logged in
-def home():
-    return render_template('home.html')
-
-@views.route("/report")  # takes you to report page
-def report():
-    return render_template('report.html')
