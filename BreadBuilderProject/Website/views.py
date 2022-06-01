4614d148c15e7650c10d2e9441469253deb11bd5
NotTheRealKeith <keith.baloyo@gmail.com>
2022-06-01 17:53:52 +1200

    Major changes + added profile page
    pages changed:
    
    - base.html
    -about.html
    -welcome.html
    -views.py
    ++profile.html (new)
    
    new components:
    -profile image for lara
    -profile image for keith

--- BreadBuilderProject/Website/views.py
+++ BreadBuilderProject/Website/views.py
@@ -9,26 +9,27 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html", user=current_user)
 
+
 @views.route('/about')
 def about():
     return render_template('about.html', user=current_user)
+
 
 @views.route('/report')
 def report():
     return render_template('report.html', user=current_user)
 
+
 @views.route('/quiz')
 def quiz():
     return render_template('quiz.html', user=current_user)
 
+
+@views.route('/profile')
+def profile():
+    return render_template('profile.html', user=current_user)
+
+
 @views.route('/')
 def welcome():
-<<<<<<< HEAD
     return render_template('welcome.html', user=current_user)
-<<<<<<< HEAD
-
-=======
->>>>>>> fabf7ffdb717deb247a6419d400e3ef1dcb2bb84
-=======
-    return render_template('welcome.html', user=current_user)
->>>>>>> parent of fabf7ff (Merge pull request #8 from NotTheRealKeith/keith)
