fbfd2f8eebd98e1ed5c0cacd2eb2bb59f99fb068
NotTheRealKeith <keith.baloyo@gmail.com>
2022-06-12 17:11:51 +1200

    profile page changes
    made changes to the profile page that allows the user to update their account details. still a wip.

--- BreadBuilderProject/Website/models.py
+++ BreadBuilderProject/Website/models.py
@@ -4,8 +4,9 @@ from . import db
 from flask_login import UserMixin
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, DateField, RadioField, HiddenField, StringField, SelectField, FloatField
-from wtforms.validators import InputRequired, Length, Regexp, NumberRange
+from wtforms.validators import InputRequired, Length, Regexp, NumberRange, DataRequired, ValidationError
 from datetime import date
+
 
 # Creating user class with database model
 
@@ -13,6 +14,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
+
 
 # Creating transaction class with transaction model
 
@@ -31,18 +33,40 @@ class Transaction(db.Model):
         self.dateDue = dateDue
         self.frequency = frequency
 
+
 # Adding the transaction input to database
 
 class AddRecord(FlaskForm):
     # id used only by update/edit
     id_field = HiddenField()
-    transType = RadioField([ InputRequired()], choices=[ ('expense', 'Expense'), ('income', 'Income') ])
-    name = StringField('Name', [ InputRequired(), Regexp(r'^[A-Za-z\s\-\'\/]+$', message="Invalid name"),
-        Length(min=1, max=30, message="Invalid name length")
-        ])
-    amount = FloatField('Amount', [ InputRequired(),
-        NumberRange(min=1, max=10000, message="Invalid range")
-        ])
-    dateDue = DateField('Date', [ InputRequired()])
-    frequency = SelectField('Frequency', [ InputRequired()],  choices=[ ('', ''), ('once', 'Once'), ('weekly', 'Weekly'), ('fortnightly', 'Fortnightly'), ('monthly', 'Monthly'), ('yearly', 'Yearly')] )
+    transType = RadioField([InputRequired()], choices=[('expense', 'Expense'), ('income', 'Income')])
+    name = StringField('Name', [InputRequired(), Regexp(r'^[A-Za-z\s\-\'\/]+$', message="Invalid name"),
+                                Length(min=1, max=30, message="Invalid name length")
+                                ])
+    amount = FloatField('Amount', [InputRequired(),
+                                   NumberRange(min=1, max=10000, message="Invalid range")
+                                   ])
+    dateDue = DateField('Date', [InputRequired()])
+    frequency = SelectField('Frequency', [InputRequired()],
+                            choices=[('', ''), ('once', 'Once'), ('weekly', 'Weekly'), ('fortnightly', 'Fortnightly'),
+                                     ('monthly', 'Monthly'), ('yearly', 'Yearly')])
     submit = SubmitField('Add Transaction')
+
+
+# Form to update user profile details
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
